ef7d3dda7159e8a4357863d4d2406eece0750cd9
tiago1820 <tiago.zdo@gmail.com>
2023-10-15 18:16:24 -0300

    Three middlewares were created, one to configure permission headers (CORS), another to parse requests with JSON format and the last to establish a base route.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const router = require("./routes/index");
+
 const server = express();
 const PORT = 3001;
+
+// Middlewares
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+});
+
+server.use(express.json());
+server.use("/rickandmorty", router);
 
 server.listen(PORT, () => {
     console.log("Server raised in port: " + PORT);
