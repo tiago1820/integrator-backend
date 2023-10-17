86a2fe661174b448d3fd3cc5cc0ad5acbf9c9563
tiago1820 <tiago.zdo@gmail.com>
2023-10-17 13:05:37 -0300

    The total number of characters are available on the route: http://localhost:3001/rickandmorty/character/count

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
-const GetCharByIdController = require("../controllers/GetCharByIdController");
-const { postFav, deleteFav } = require("../controllers/handleFavorites");
-const AuthController = require("../controllers/AuthController");
+const GetCharByIdController = require("../controllers/getCharByIdController");
+const FavoritesController = require("../controllers/favoritesController");
+const AuthController = require("../controllers/authController");
 
+router.get("/character/count", GetCharByIdController.getTotalCharacterCount.bind(GetCharByIdController));
 router.get("/character/:id", GetCharByIdController.handle.bind(GetCharByIdController));
-router.post("/fav", postFav);
-router.delete("/fav/:id", deleteFav);
+
+router.post("/fav", FavoritesController.postFav.bind(FavoritesController));
+router.delete("/fav/:id", FavoritesController.deleteFav.bind(FavoritesController));
 
 router.get("/login", AuthController.login.bind(AuthController));
 router.get("/register", AuthController.register.bind(AuthController));
